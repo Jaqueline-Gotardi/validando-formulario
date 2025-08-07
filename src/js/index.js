4d90a80355ff7976c12bafa7c8c7930a2a86a44f
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-08-07 17:29:03 -0500

    enviando js + ajustes no css

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
- 
+const formulario = document.getElementById('formulario');
+const campos = document.querySelectorAll('#formulario input , #formulario textarea');
+const mensagensErro = document.querySelectorAll('#formulario .campo .mensagem-erro');
+
+
+ formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); /* evita a validação antes de analisar */
+    validarFormulario();
+ });
+
+ function validarFormulario() {
+    campos.forEach((input, index) => {
+        if(input.value.trim() === '') {
+            input.classList.add('erro');
+            mensagensErro[index].classList.add('ativo');
+
+        } else {
+            input.classList.add('certo');
+            input.classList.remove('erro');
+            mensagensErro[index].classList.remove('ativo');
+        }
+    }    
+)};
